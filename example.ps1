35dd7cbaabffa9eee77425ae00e7e19e0e48b0ef
Miodrag Milic <miodrag.milic@gmail.com>
2014-04-02 20:00:41 +0200

    example ps1

---
+++ example.ps1
@@ -0,0 +1,15 @@
+. "$PSScriptRoot\inc\flea.ps1"
+
+flea @{
+    freq       = 10;
+    backends   = @(
+        ,(file "out.txt")
+        ,(console yellow red)
+    );
+    debug      = 1;
+    monitors   = @(
+            , ("cpu_load", -1,     "cpu_load",  9)
+            , ("ram_free", 6,      "ram_free")
+            , ("disk_c",   '*/1m', "disk_free", "c:")
+    )
+}
